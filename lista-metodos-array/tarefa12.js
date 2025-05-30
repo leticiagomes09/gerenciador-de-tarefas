191c082473d7e59481771d9d2c181a3bd70b30be
leticiagomes09 <leticia.m.gomes10@aluno.senai.br>
2025-05-30 11:15:02 -0300

    feat: Ordena tarefas

---
+++ lista-metodos-array/tarefa12.js
@@ -0,0 +1,5 @@
+let tarefas = ['Estudar JavaScript', 'Fazer exercícios de matemática', 'Lavar a louça', 'Comprar mercado', 'Ir à academia', 'Ler um livro', 'Fazer revisão para o teste', 'Limpar o quarto', 'Estudar para concurso', 'Organizar o computador'];
+
+// Ordenando em ordem alfabética
+let tarefasOrdenada = tarefas.sort();
+console.log("Lista de tarefas ordenada:", tarefasOrdenada);
