5f2b05e870f24e44579b7340d411df65c2050441
leticiagomes09 <leticia.m.gomes10@aluno.senai.br>
2025-05-30 08:59:39 -0300

    feat: Remove última tarefa

--- lista-metodos-array/terefa02.js
+++ lista-metodos-array/terefa02.js
@@ -1,1 +1,12 @@
-//01 
+let tarefas = ['Estudar JavaScript', 'Fazer exercícios de matemática', 'Lavar a louça', 'Comprar mercado', 'Ir à academia', 'Ler um livro', 'Fazer revisão para o teste', 'Limpar o quarto', 'Estudar para concurso', 'Organizar o computador'];
+
+console.log("Antes do pop");
+console.table(tarefas);
+
+// Removendo a ultima tarefa 
+let tarefaRemovida = tarefas.pop();
+
+console.log("Depois do pop");
+console.table(tarefas);
+
+console.log("Tarefa Removida: ", tarefaRemovida);
