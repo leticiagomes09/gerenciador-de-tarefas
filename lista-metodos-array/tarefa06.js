c8ddca71421366d75314b044307729954588fc5e
leticiagomes09 <leticia.m.gomes10@aluno.senai.br>
2025-05-30 09:51:02 -0300

    feat: Transforma a lista de tarefas em string

---
+++ lista-metodos-array/tarefa06.js
@@ -0,0 +1,4 @@
+let tarefas = ['Estudar JavaScript', 'Fazer exercícios de matemática', 'Lavar a louça', 'Comprar mercado', 'Ir à academia', 'Ler um livro', 'Fazer revisão para o teste', 'Limpar o quarto', 'Estudar para concurso', 'Organizar o computador'];
+
+// Transformando em strings separadas por vígula
+console.log("tarefas como string: ", tarefas.join(", "));
